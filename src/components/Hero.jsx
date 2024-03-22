0b167306965ee4297bca3f95c972f96850c33a22
Abrham <abrsh6266@gmail.com>
2024-03-22 11:30:20 -0800

    adding hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,36 @@
+import heroImg from '../assets/hero.svg';
+import { FaGithubSquare, FaLinkedin, FaReact, FaTwitterSquare } from 'react-icons/fa';
+
+const Hero = () => {
+    return (
+      <section className='bg-emerald-100 py-24  '>
+        <div className=' mx-auto max-w-7xl  px-8  grid  md:grid-cols-2 items-center gap-8'>
+          <article>
+            <h1 className='text-7xl font-bold tracking-wider'>I'm Abrham</h1>
+            <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>
+              Full stack developer
+            </p>
+            <p className='mt-2 text-lg text-slate-700 capitalize tracking-wide'>
+              turning ideas into interactive reality
+            </p>
+            <div className='flex gap-x-4 mt-4'>
+              <a href='https://github.com/abrsh6266'>
+                <FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+              </a>
+              <a href='https://www.linkedin.com/in/abrham-belayineh-190658258'>
+                <FaLinkedin className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+              </a>
+              <a href='https://contentful-cms-livid.vercel.app/'>
+                <FaReact className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+              </a>
+            </div>
+          </article>
+  
+          <article className='hidden md:block '>
+            <img src={heroImg} className='h-80 lg:h-96' />
+          </article>
+        </div>
+      </section>
+    );
+  };
+  export default Hero;
